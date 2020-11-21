e603ef7e35f1f74c179ef7bfaa9a3e844f44dc22
Dustin Jiang <dustinjiang@outlook.com>
2020-11-21 10:26:23 +0800

    [Modify]Modify Getch() in  crossSystem.py

--- crossSystem.py
+++ crossSystem.py
@@ -47,21 +47,21 @@ class CrossSys:
    
 #Class for getchar() 
 def Getch():
-    def _GetchUnix():
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-    def _GetchWindows():
-        import msvcrt
-        return msvcrt.getch()
+  def _GetchUnix():
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
     try:
-        impl = _GetchWindows()
-    except ImportError:
-        impl = _GetchUnix()
-    return impl
+      tty.setraw(sys.stdin.fileno())
+      ch = sys.stdin.read(1)
+    finally:
+      termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+  def _GetchWindows():
+    import msvcrt
+    return msvcrt.getch()
+  try:
+    impl = _GetchWindows()
+  except ImportError:
+    impl = _GetchUnix()
+  return impl
